6a5efbe973283550c36145768a120ab8b195b8a3
Glen Bangkila <hadeinvane@gmail.com>
2026-09-25 09:57:40 +0800

    chore(release): v0.1.3

--- Project.swift
+++ Project.swift
@@ -26,7 +26,7 @@ let project = Project(
                 "LSUIElement": true,
                 "CFBundleName": "Token Usage",
                 "CFBundleDisplayName": "Token Usage",
-                "CFBundleShortVersionString": "0.1.2",
+                "CFBundleShortVersionString": "0.1.3",
                 "LSMultipleInstancesProhibited": true,
                 "LSApplicationCategoryType": "public.app-category.developer-tools",
             ]),
